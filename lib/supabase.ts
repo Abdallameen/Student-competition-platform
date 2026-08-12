d8c30309aca78bc8b4b94bc341979bfca455dbda
Abdallameen <abdallahameen94@gmail.com>
2026-08-13 00:17:18 +0300

    Remove comment about new keys in supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-// استخدام المفاتيح الجديدة
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://xvmuaolidigamnnlijox.supabase.co'
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'sb_publishable_4JpE51qL_gEMsm3xBPJcaQ_n3YzNCyH'
 
